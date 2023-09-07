f6b710b6a3c7ebef83a7485337add68e8459285b
juankaplan <jkaplan@udesa.edu.ar>
2023-09-07 20:49:37 -0300

    graficos de error y comparaciones

--- tp1/1_granja.py
+++ tp1/1_granja.py
@@ -7,6 +7,15 @@ import numpy as np
 mediciones_file = "mnyo_mediciones.csv"
 ground_truth = "mnyo_ground_truth.csv"
 
+def create_error_graph(axs, functions, title=""):
+    for function in functions:
+        axs.plot(function[0], function[1], '-', label= function[2])
+
+    axs.set_xlabel('X')
+    axs.set_ylabel('Y')
+    axs.legend()
+    axs.grid(True)
+    return axs
 
 def create_line_graph(axs, x_datapoints, y_datapoints, functions, title=""):
     axs.plot(x_datapoints, y_datapoints, "o", label="Data Points")
@@ -94,8 +103,10 @@ t_intersection_value3, iterations_3 = newtonRaphson(linear_intersection, linear_
 t_intersection_value4, iterations_4 = newtonRaphson(linear_intersection, linear_intersection_deriv, 2, 0.001)
 
 print(t_intersection_value2)
-x1_intersections = [x1_interpolated(t_intersection_value1), x1_interpolated(t_intersection_value2), x1_interpolated(t_intersection_value3), x1_interpolated(t_intersection_value4)]
-x2_intersections = [x2_interpolated(t_intersection_value1), x2_interpolated(t_intersection_value2), x2_interpolated(t_intersection_value3), x2_interpolated(t_intersection_value4)]
+x1_intersections = [x1_interpolated(t_intersection_value2), x1_interpolated(t_intersection_value3), x1_interpolated(t_intersection_value4),x1_interpolated(t_intersection_value1)]
+x2_intersections = [x2_interpolated(t_intersection_value2), x2_interpolated(t_intersection_value3), x2_interpolated(t_intersection_value4),x2_interpolated(t_intersection_value1),]
+
+list1 = [t_intersection_value1, t_intersection_value2, t_intersection_value3, t_intersection_value4]
 plt.plot(x1_intersections, x2_intersections, "o", label = "Intersection")
 
 plt.legend()
@@ -123,4 +134,44 @@ axs_convergence.set(xlabel="Iteration", ylabel="Pn - Pn-1")
 plt.legend()
 
 # Show the convergence plot
+plt.show()
+
+# Encontrar los verdaderos puntos de interseccion
+# Interpolacion de la ground truth para aproximar valores de por medio
+
+time_values = list(np.linspace(0, 9, 100))
+print(len(time_values))
+x1_truth = CubicSpline(time_values, x1_groundtruth)
+x2_truth = CubicSpline(time_values, x2_groundtruth)
+
+def x10_truth_poly(t):
+    return (x1_truth(t) - 10)
+
+def linear_truth_intersection(t):
+    return x2_truth(t) + 0.35 * x1_truth(t) - 3.6
+
+def linear_truth_intersection_deriv(t):
+    return 0.35 * x1_truth.derivative()(t) + x2_truth.derivative()(t)
+
+t_intersection_value5, iterations_5 = newtonRaphson(x10_truth_poly, x1_truth.derivative(), 3, 0.001)
+t_intersection_value6, iterations_6 = newtonRaphson(linear_truth_intersection, linear_truth_intersection_deriv, 0, 0.001)
+t_intersection_value7, iterations_7 = newtonRaphson(linear_truth_intersection, linear_truth_intersection_deriv, 1.5, 0.001)
+t_intersection_value8, iterations_8 = newtonRaphson(linear_truth_intersection, linear_truth_intersection_deriv, 2.5, 0.001)
+
+print(t_intersection_value6, t_intersection_value7, t_intersection_value8, t_intersection_value5)
+x1_intersections2 = [x1_truth(t_intersection_value6), x1_truth(t_intersection_value7), x1_truth(t_intersection_value8),x1_truth(t_intersection_value5)]
+x2_intersections2 = [x2_truth(t_intersection_value6), x2_truth(t_intersection_value7), x2_truth(t_intersection_value8),x2_truth(t_intersection_value5),]
+print(x1_intersections)
+print(x1_intersections2)
+print(x2_intersections)
+print(x2_intersections2)
+
+#Ver comparacion de error entre las dos
+x1_original_data = x1_truth(t_interval)
+x2_original_data = x2_truth(t_interval)
+error_x1 = np.abs(x1_original_data - x1_interp_data)
+error_x2 = np.abs(x2_original_data - x2_interp_data)
+
+fig, axs_error = plt.subplots(figsize=(10, 6))
+create_error_graph(axs_error, [(t_interval, error_x1, "x1_error"), (t_interval, error_x2, "x2_error")])
 plt.show()
